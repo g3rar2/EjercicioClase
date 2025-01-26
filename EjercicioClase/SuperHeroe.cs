bcae65fd1207966b3eb1dc62df09006359d9c3cc
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-01-25 18:32:34 -0600

    add class SuperHeroe

---
+++ EjercicioClase/SuperHeroe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase
+{
+    internal class SuperHeroe
+    {
+        public string nombre { get; set; }
+        public string identidadSecreta { get; set; }
+        public string ciudad {  get; set; }
+        public Boolean puedeVolar {  get; set; }
+
+    }
+}
