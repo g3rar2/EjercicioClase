3aa50d1aac28fcf40cbe1888f84b7b69829c091a
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-01-25 18:48:53 -0600

    add superPoder object in SuperHeroe class

--- EjercicioClase/SuperHeroe.cs
+++ EjercicioClase/SuperHeroe.cs
@@ -8,10 +8,18 @@ namespace EjercicioClase
 {
     internal class SuperHeroe
     {
+        
         public string nombre { get; set; }
         public string identidadSecreta { get; set; }
         public string ciudad {  get; set; }
         public Boolean puedeVolar {  get; set; }
+
+        public Superpoder superPoder { get; set; }
+
+        public SuperHeroe() {
+            superPoder = new Superpoder();
+
+        }
 
     }
 }
