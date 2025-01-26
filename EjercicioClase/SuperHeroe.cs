cf45ca6e294b3423b065bc69b54023d2a3260b6f
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-01-25 18:55:26 -0600

    add Imprimir method

--- EjercicioClase/SuperHeroe.cs
+++ EjercicioClase/SuperHeroe.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace EjercicioClase
 {
@@ -12,13 +14,25 @@ namespace EjercicioClase
         public string nombre { get; set; }
         public string identidadSecreta { get; set; }
         public string ciudad {  get; set; }
-        public Boolean puedeVolar {  get; set; }
+        public bool puedeVolar {  get; set; }
 
         public Superpoder superPoder { get; set; }
 
         public SuperHeroe() {
             superPoder = new Superpoder();
+        }
 
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {nombre}");
+            Console.WriteLine($"Identidad Secreta: {identidadSecreta}");
+            Console.WriteLine($"Ciudad: {ciudad}");
+            Console.WriteLine($"Puede volar:: {puedeVolar}");
+            Console.WriteLine($"Super poder: {superPoder.nombre}");
+            Console.WriteLine($"Descripcion: {superPoder.descripcion}");
+            Console.WriteLine($"Nivel: {superPoder.nivel}");
+            Console.WriteLine();
+            Console.WriteLine();
         }
 
     }
